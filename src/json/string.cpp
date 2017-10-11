d6c308135924a790634aa8da626bc3afb8e9ce02
Tymoteusz Blazejczyk <tymoteuszblazejczyk@benetel.com>
2017-10-11 16:23:28 +0200

    String imroved clean up

--- src/json/string.cpp
+++ src/json/string.cpp
@@ -67,6 +67,52 @@ String& String::insert(size_type index, const_pointer s) noexcept {
     return *this;
 }
 
+String::String(const String& other) noexcept :
+    String{other, const_cast<String&>(other).allocator()}
+{ }
+
+String::String(const String& other, allocator_type& alloc) noexcept :
+    m_allocator{&alloc}
+{
+    insert(0, other);
+}
+
+String::String(String&& other, allocator_type& alloc) noexcept :
+    m_allocator{&alloc}
+{
+    assign(std::move(other));
+}
+
+String::String(allocator_type& alloc) noexcept :
+    m_allocator{&alloc}
+{ }
+
+String::String(const String& other, size_type pos, size_type count,
+        allocator_type& alloc) noexcept :
+    m_allocator{&alloc}
+{
+    insert(0, other, pos, count);
+}
+
+String::String(size_type count, value_type ch, allocator_type& alloc) noexcept :
+    m_allocator{&alloc}
+{
+    insert(size_type(0), count, ch);
+}
+
+String::String(const_pointer s, allocator_type& alloc) noexcept :
+    m_allocator{&alloc}
+{
+    insert(0, s);
+}
+
+String::String(const_pointer s, size_type count,
+        allocator_type& alloc) noexcept :
+    m_allocator{&alloc}
+{
+    insert(0, s, count);
+}
+
 String::String(String&& other) noexcept {
     assign(std::move(other));
 }
@@ -209,6 +255,11 @@ String::const_pointer String::c_str() noexcept {
     return str;
 }
 
+auto String::erase(const_iterator first,
+        const_iterator last) noexcept -> iterator {
+    return {};
+}
+
 String& String::erase(size_type index, size_type count) noexcept {
     if (index <= size()) {
         auto total_size = size() - index;
@@ -223,4 +274,230 @@ String& String::erase(size_type index, size_type count) noexcept {
         m_size -= count;
     }
     return *this;
+}
+
+auto String::operator=(const String& other) noexcept -> String& {
+    return assign(other);
+}
+
+auto String::operator=(String&& other) noexcept -> String& {
+    return assign(other);
+}
+
+auto String::operator=(const_pointer s) noexcept -> String& {
+    return assign(s);
+}
+
+auto String::operator=(value_type ch) noexcept -> String& {
+    return assign(1, ch);
+}
+
+auto String::operator=(std::initializer_list<value_type> ilist) noexcept -> String& {
+    return assign(ilist);
+}
+
+auto String::assign(const String& other) noexcept -> String& {
+    return assign(other.data(), other.size());
+}
+
+auto String::assign(const_pointer s) noexcept -> String& {
+    return assign(s, length(s));
+}
+
+auto String::assign(size_type count, value_type ch) noexcept -> String& {
+    clear();
+    insert(0, StringView{&ch, count}, fill_n);
+    return *this;
+}
+
+auto String::assign(const String& other, size_type pos,
+        size_type count) noexcept -> String& {
+    clear();
+    insert(0, StringView{other}.subspan(pos, count), copy_n);
+    return *this;
+}
+
+auto String::assign(const_pointer s, size_type count) noexcept -> String& {
+    clear();
+    insert(0, StringView{s, count}, copy_n);
+    return *this;
+}
+
+auto String::assign(std::initializer_list<value_type> ilist) noexcept ->
+        String& {
+    return assign(ilist.begin(), ilist.size());
+}
+
+auto String::assign(const_iterator first,
+        const_iterator last) noexcept -> String& {
+    clear();
+    insert(0, StringView{first, last}, copy_n);
+    return *this;
+}
+
+auto String::at(size_type pos) noexcept -> reference {
+    return m_data[pos];
+}
+
+auto String::at(size_type pos) const noexcept -> const_reference {
+    return m_data[pos];
+}
+
+auto String::operator[](size_type pos) noexcept -> reference {
+    return m_data[pos];
+}
+
+auto String::operator[](size_type pos) const noexcept -> const_reference {
+    return m_data[pos];
+}
+
+auto String::front() noexcept -> reference {
+    return m_data[0];
+}
+
+auto String::front() const noexcept -> const_reference {
+    return m_data[0];
+}
+
+auto String::back() noexcept -> reference {
+    return m_data[size() - 1];
+}
+
+auto String::back() const noexcept -> const_reference {
+    return m_data[size() - 1];
+}
+
+auto String::data() noexcept -> pointer {
+    return m_data;
+}
+
+auto String::data() const noexcept -> const_pointer {
+    return m_data;
+}
+
+auto String::begin() noexcept -> iterator {
+    return m_data;
+}
+
+auto String::begin() const noexcept -> const_iterator {
+    return m_data;
+}
+
+auto String::cbegin() const noexcept -> const_iterator {
+    return m_data;
+}
+
+auto String::end() noexcept -> iterator {
+    return m_data + m_size;
+}
+
+auto String::end() const noexcept -> const_iterator {
+    return m_data + m_size;
+}
+
+auto String::cend() const noexcept -> const_iterator {
+    return m_data + m_size;
+}
+
+auto String::rbegin() noexcept -> reverse_iterator {
+    return reverse_iterator(end() - 1);
+}
+
+auto String::rbegin() const noexcept -> const_reverse_iterator {
+    return const_reverse_iterator(cend() - 1);
+}
+
+auto String::crbegin() const noexcept -> const_reverse_iterator {
+    return const_reverse_iterator(cend() - 1);
+}
+
+auto String::rend() noexcept -> reverse_iterator {
+    return reverse_iterator(begin() - 1);
+}
+
+auto String::rend() const noexcept -> const_reverse_iterator {
+    return const_reverse_iterator(cbegin() - 1);
+}
+
+auto String::crend() const noexcept -> const_reverse_iterator {
+    return const_reverse_iterator(cbegin() - 1);
+}
+
+auto String::empty() const noexcept -> bool {
+    return (0 == m_size);
+}
+
+auto String::size() const noexcept -> size_type {
+    return m_size;
+}
+
+auto String::length() const noexcept -> size_type {
+    return m_size;
+}
+
+void String::pop_back() noexcept {
+    if (!empty()) {
+        --m_size;
+    }
+}
+
+void String::clear() noexcept {
+    m_size = 0;
+}
+
+auto String::insert(size_type index, const String& str) noexcept -> String& {
+    return insert(index, str.data(), str.size());
+}
+
+auto String::insert(const_iterator pos, value_type ch) noexcept -> iterator {
+    return insert(pos, 1, ch);
+}
+
+auto String::insert(const_iterator pos,
+        std::initializer_list<value_type> ilist) noexcept -> String& {
+    return insert(size_type(pos - cbegin()), ilist.begin(), ilist.size());
+}
+
+auto String::insert(size_type index, size_type count,
+        value_type ch) noexcept -> String& {
+    insert(index, StringView{&ch, count}, fill_n);
+    return *this;
+}
+
+auto String::insert(size_type index, const_pointer s,
+        size_type count) noexcept -> String& {
+    insert(index, StringView{s, count}, copy_n);
+    return *this;
+}
+
+auto String::insert(size_type index, const String& str,
+        size_type index_str, size_type count) noexcept -> String& {
+    insert(index, StringView{str}.subspan(index_str, count), copy_n);
+    return *this;
+}
+
+auto String::insert(const_iterator pos,
+        const_iterator first, const_iterator last) noexcept -> iterator {
+    return insert(size_type(pos - cbegin()), StringView{first, last}, copy_n);
+}
+
+auto String::insert(const_iterator pos, size_type count,
+        value_type ch) noexcept -> iterator {
+    return insert(size_type(pos - cbegin()), StringView{&ch, count}, fill_n);
+}
+
+auto String::erase(const_iterator position) noexcept -> iterator {
+    return erase(position, position + 1);
+}
+
+auto String::allocator() noexcept -> allocator_type& {
+    return *m_allocator;
+}
+
+auto String::allocator() const noexcept -> const allocator_type& {
+    return *m_allocator;
+}
+
+String::operator StringView() const noexcept {
+    return StringView{m_data, m_size};
 }
